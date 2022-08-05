11699caba920eb012a63388a85fba4003a49ef10
Banigo Kenebebh <kenebebhbanigo@gmail.com>
2022-08-05 05:19:02 +0100

    added js functionaality

--- transfer.js
+++ transfer.js
@@ -4,11 +4,20 @@ transfers.forEach((transfer) => transfer.classList.add("hidden"));
 
 const setPinButton = document.querySelector(".setpin-button");
 const setPinContainer = document.querySelector(".pin-box__container");
+
+const backButton = document.querySelector(".back-button");
+
 const transferPage1 = document.querySelector(".transfer-page1");
 const transferPage2 = document.querySelector(".transfer-page2");
 const transferPage3 = document.querySelector(".transfer-page3");
 const transferPage4 = document.querySelector(".transfer-page4");
+const transferPage5 = document.querySelector(".transfer-page5");
+
 let pageNumber = 1;
+
+backButton.addEventListener("click", function () {
+  location.href = "index.html";
+});
 
 setPinButton.addEventListener("click", function (e) {
   e.preventDefault();
@@ -45,6 +54,15 @@ transferPage3.addEventListener("click", function (e) {
   pageNumber = 3;
 
   if (e.target.classList.contains("send-money-1")) {
+    changeActivePage();
+  }
+});
+
+transferPage4.addEventListener("click", function (e) {
+  e.preventDefault();
+  pageNumber = 4;
+
+  if (e.target.classList.contains("confirm-transfer")) {
     changeActivePage();
   }
 });
